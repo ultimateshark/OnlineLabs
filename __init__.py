1f2343e9adcbbb4b30b03541750729be8ebeb718
Manish Pandey <37942264+ManishPandeyCSE@users.noreply.github.com>
2018-09-17 22:02:37 -1200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,8 +11,8 @@ app=Flask(__name__)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT']=465
-app.config['MAIL_USERNAME']='in.hodophile@gmail.com'
-app.config['MAIL_PASSWORD']='rajatmanish'
+app.config['MAIL_USERNAME']='example@gmail.com'
+app.config['MAIL_PASSWORD']='*********'
 app.config['MAIL_USE_TLS']=False
 app.config['MAIL_USE_SSL']=True
 mail=Mail(app)
